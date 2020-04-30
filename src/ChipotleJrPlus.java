ebbb3a31e9b7bc3872ae5ec9b4dce385b3bb6ee6
ashuashenafi <ashuashenafi@gmail.com>
2020-04-30 12:14:45 -0400

    better formating, ready for receipt

--- src/ChipotleJrPlus.java
+++ src/ChipotleJrPlus.java
@@ -1,9 +1,17 @@
+import com.sun.istack.internal.NotNull;
+
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
+import static java.lang.String.format;
+
 public class ChipotleJrPlus {
+
+    public static DecimalFormat df2 = new DecimalFormat("#.##");
+
     public static void main(String[] args) {
 
         // initialize Random class
@@ -69,8 +77,8 @@ public class ChipotleJrPlus {
         sourcream.add("no");
 
         // replace strings like "no", "none", "all", "yes"
-        rice.set(0, "brown rice");
-        rice.set(1,"white rice");
+        rice.set(0, "white rice");
+        rice.set(1, "brown rice");
         rice.set(2, "no rice");
         rice.set(3, "all rice");
 
@@ -89,7 +97,7 @@ public class ChipotleJrPlus {
         salsa.set(4, "all salsa");
 
         veggies.set(2, "no veggies");
-        veggies.set(3, "all salsa");
+        veggies.set(3, "all veggies");
 
         cheese.set(0, "cheese");
         cheese.set(1, "no cheese");
@@ -103,8 +111,17 @@ public class ChipotleJrPlus {
         sourcream.set(0, "sour cream");
         sourcream.set(1, "no sour cream");
 
+        // declare arraylists for summary count purpose
+        int[][] all_count = {{0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}};
+        double all_price = 0;
+
+        ArrayList<String> burrito_for_summary = new ArrayList<>();
+
+// ******************************************** FOR LOOP for 25 Burritos ********************************************
 
         for (int num_burrido = 0; num_burrido < 25; num_burrido++) {
+            // arraylist of index of each component for cashier
+            ArrayList<String> burrito_for_cashier = new ArrayList<>();
 
             // integer variable to store quntity & price of burrido
 
@@ -116,8 +133,6 @@ public class ChipotleJrPlus {
 
             // arraylist of index of each specific component among the ingredients
             ArrayList<Integer> burrito_component = new ArrayList<>();
-            // arraylist of index of each component for cashier
-            ArrayList<String> burrito_for_cashier = new ArrayList<>();
 
             // generate a random number to decide how many ingredients
             // between 5 to (9+1) ... rnd_high = 10 and rnd_low = 5;
@@ -145,7 +160,6 @@ public class ChipotleJrPlus {
                 }   // end if loop
             }   // end for loop
             bubblesort(index_ingredients);
-//            System.out.println("index_ingredients = " + index_ingredients);
 
             for (int i = 0; i < how_many_ingredients; i++) {
 
@@ -159,7 +173,6 @@ public class ChipotleJrPlus {
                 burrito_component.add(temp);
             }   // end for loop
 
-
             for (int i = 0; i < how_many_ingredients; i++) {
 
                 String temp = all_ingredients.get(index_ingredients.get(i)).get(burrito_component.get(i));
@@ -169,8 +182,10 @@ public class ChipotleJrPlus {
 
             //calculate rate of each ingredient
             for (int i = 0; i < burrito_for_cashier.size(); i++) {
+
                 boolean temp_no = false;
                 boolean temp_none = false;
+
                 temp_no = burrito_for_cashier.get(i).contains("no");
                 temp_none = burrito_for_cashier.get(i).contains("none");
 
@@ -180,52 +195,111 @@ public class ChipotleJrPlus {
                     qty_for_rate++;
                 }   // end if loop
             }   // end of for loop
-            burrito_price = 3 + (qty_for_rate * 0.5);
-            System.out.printf("\nBurrito %d: ", (num_burrido + 1));
-//            System.out.println("burrito_component = " + burrito_component);
 
+// *************************************  OUTPUT FOR ONE BURRITO ************************************************
+
+            String one_burrito ="";
+
+            burrito_price = 3 + (qty_for_rate * 0.5);           // price for one burrito
+
+            one_burrito += "\nBurrito : " + (num_burrido + 1);
+
+            // format the output for one burrito ... ummm smelling!!!!!!!!  already
             for (int i = 0; i < burrito_for_cashier.size(); i++) {
 
                 if (i > 0) {
-                    System.out.print(", ");
+                    // print comma after the first item good formating
+                    one_burrito += ", ";
                 }
 
-
-                System.out.printf("%s ", burrito_for_cashier.get(i));
+                one_burrito += " " + burrito_for_cashier.get(i);
             }
+            all_price += burrito_price;
 
-            System.out.printf("\t\t$%.2f", burrito_price);
-            System.out.print("\n--------------------------------------------------------------------------------------");
+            System.out.println(format("%-130s %10s", one_burrito, format("$%(,.2f", burrito_price)));
 
-//        System.out.println("\n burrito for cashier = " + burrito_for_cashier);
-        }   // end of for loop
+            print_dashes('-', 140);
+
+            // copy the arraylist into another arraylist to calculate the 25 total burritos.
+            burrito_for_summary.addAll(burrito_for_cashier);
+
+        }   // end of for loop for 25 Burritos
+
+        String print_summary = "";
+        int count = 0;
+
+        for (int k = 0; k < burrito_for_summary.size(); k++) {
+
+            for (int i = 0; i < all_ingredients.size(); i++) {
+                for (int j = 0; j < all_ingredients.get(i).size(); j++) {
+
+                    boolean temp1 = burrito_for_summary.get(k).equals(all_ingredients.get(i).get(j));
+                    boolean temp2 = !(burrito_for_summary.get(k).contains("no"));       // this represents both "no" and "none"
+                    if (temp1 && temp2) {
+                        all_count[i][j] = (all_count[i][j] + 1);
+                    }
+                }
+            }
+        }
+
+// OUTPUT OUTPUT **************** SUMMARY SENTENCE **************** OUTPUT OUTPUT!
+        
+        print_summary += "\nThis order has ";
+        for (int i = 0; i < all_count.length; i++) {
+            for (int j = 0; (all_count[i] != null && j < all_count[i].length); j++) {
+
+                if (all_count[i][j] > 0) {
+                    if (count > 0) {
+                        print_summary += ", ";
+                    }
+
+                    print_summary += (all_count[i][j] + " " + all_ingredients.get(i).get(j));
+                    count++;
+                }
+            }
+        }
+
+        print_summary += ", and the sum is $" + format("%.2f", all_price) + ".";
+
+        String str = print_summary;
+        // limit number of characters per line
+        String parsedStr = str.replaceAll("(.{50})", "$1\n");
+        System.out.println(parsedStr);
 
     }   // end of main()
 
     // a method to generate random number
-    public static int generate_random(int max, int min){
+    public static int generate_random(int max, int min) {
         // initialize Random class
         Random rnd = new Random();
 
-        if(max == 0){
+        if (max == 0) {
             max++;
         }
         int result = min + rnd.nextInt(max - min);
         return result;
     }
+
     // method to sort results
-    public static void bubblesort(ArrayList <Integer> sorted_alist){
+    public static void bubblesort(ArrayList<Integer> sorted_alist) {
         int n1 = sorted_alist.size();
-        for(int i = 0; i < n1-1; i++){
-            for(int j = 0; j < n1-i-1; j++){
-                int a = sorted_alist.get(j+1);
+        for (int i = 0; i < n1 - 1; i++) {
+            for (int j = 0; j < n1 - i - 1; j++) {
+                int a = sorted_alist.get(j + 1);
                 int b = sorted_alist.get(j);
                 if (a < b) {
                     int temp = a;
-                    sorted_alist.set(j+1, b);
+                    sorted_alist.set(j + 1, b);
                     sorted_alist.set(j, temp);
                 }
             }
         }
     }   // end of bubblesort() class
+
+    public static void print_dashes(char c, int n){
+        for(int i = 0; i < n; i++){
+            System.out.print("-");
+        }
+    }
+
 }   // end of class
